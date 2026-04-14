1b837e7e357eefa3e8934d8a56ef288a87356fbe
fosho <fayokunmiosho@gmail.com>
2026-04-14 17:07:00 +0100

    complete chapter 3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,26 +12,17 @@ type application struct {
 }
 
 func main() {
-	
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	app := &application{
 		logger: logger,
 	}
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	mux := http.NewServeMux()
-	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /", app.home)
-	mux.HandleFunc("GET /snippet/view", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	// log.Printf("Starting Server at PORT %s", *addr)
 	logger.Info("Starting Server at PORT", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 
