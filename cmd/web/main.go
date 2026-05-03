629c9a5073624c34985b4ff696435117812f4768
Fayorr <fayokunmiosho@gmail.com>
2026-05-03 18:11:44 +0100

    add database model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
 	logger *slog.Logger
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:nuttertools@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		// AddSource: true,
+	}))
+
+	db, err := openDB(*dsn) 
+	
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	db.Close()
+
 	app := &application{
 		logger: logger,
 	}
 
 	app.logger.Info("Starting Server at PORT", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+
+	err = http.ListenAndServe(*addr, app.routes())
 	app.logger.Error(err.Error())
 	os.Exit(1)
 
